b578dad4e54d27ba794ecf76db60befa26c2c71d
felpino2 <felipeafa202@gmail.com>
2026-08-31 18:27:11 -0300

    weird algorithm implemented

---
+++ CSES/1068-weirdAlgorithm.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+
+    long long a;
+    cin >> a;
+    
+    cout << a << " ";
+    while (a!=1){
+        if (a%2 == 0){
+            a = a/2; 
+        } else {
+            a = (a*3)+1;
+        }
+        cout << a << " ";
+    }
+    
+    return 0;
+}
